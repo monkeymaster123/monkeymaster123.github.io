4b53eedb7153e3f6dd274f2f0c3f1ae32427648e
Dalton Craven <20dalton00@gmail.com>
2017-05-23 00:58:24 +0000

    add slack call

--- contact/google-sheet.js
+++ contact/google-sheet.js
@@ -1,5 +1,6 @@
 // Variable to hold request
 var request;
+var slack;
 var email;
 
 // Bind to the submit event of our form
@@ -22,8 +23,49 @@ $("#contact").submit(function(event){
     // Note: we disable elements AFTER the form data has been serialized.
     // Disabled form elements will not be serialized.
     $inputs.prop("disabled", true);
+    
 
     // Fire off the request to /form.php
+    
+    
+    
+    // var url = "https://hooks.slack.com/services/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";
+    slack = $.ajax({
+        data: 'payload=' + JSON.stringify({
+          	"fallback": "New Form Submission--Dalton Craven",
+          	
+        	"pretext": "New Form Submission!",
+        	"color": "#36a64f",
+        	"fields": [
+        		{
+        			"title": "Full Name",
+        			"value": "Dalton Craven",
+        			"short": true
+        		},
+        		{
+        			"title": "Email",
+        			"value": "20dalton00@gmail.com",
+        			"short": true
+        		},
+        		{
+        			"title": "Subject",
+        			"value": "Hiring You",
+        			"short": false
+        		},
+        		{
+        			"title": "Message",
+        			"value": "I'd like to hire you. I think that you're super cool and you're great",
+        			"short": true
+        		}
+        	]
+            
+        }),
+        dataType: 'json',
+        processData: false,
+        type: 'POST',
+        url: "https://hooks.slack.com/services/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"
+    });
+    
     request = $.ajax({
         url: "https://script.google.com/macros/s/AKfycbx57dI3ifzS_YrCcwjLOJr3HVfkleLsXc6-_gjn72k57xZsOxD3/exec",
         type: "post",
@@ -37,12 +79,6 @@ $("#contact").submit(function(event){
         console.log(response);
         console.log(textStatus);
         console.log(jqXHR);
-        // email = $.ajax({
-        //     url: "https://formspree.io/graffcrafters@gmail.com", 
-        //     method: "POST",
-        //     data: serializedData,
-        //     dataType: "json"
-        // });
         window.location = 'thanks.html';
         
         
